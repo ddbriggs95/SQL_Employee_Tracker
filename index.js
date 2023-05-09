d8168fa212008b6a27526cc9b7ee9f9748001b1a
ddbriggs95 <dowlingbriggs@gmail.com>
2023-05-09 18:37:05 -0500

    Created db connection

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 //Dependencies
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+
+//Connect to a database
+const db = mysql.createConnection( {
+    host: 'localhost',
+    user: 'root',
+    password: 'serenity123',
+    database: 'employee_tracker_db'
+});
+
+
+//prompts for user to view the database data
