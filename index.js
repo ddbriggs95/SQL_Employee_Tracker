e33440fd2fb75b69b1580ff56d133214e7fd1bee
ddbriggs95 <dowlingbriggs@gmail.com>
2023-05-11 21:39:29 -0500

    Added log out and add employee functions

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const db = mysql.createConnection({
 );
 
 
-//inquirer prompts for user to view the database data by responding to choices
+//inquirer prompts for user to view the database data or perform an action by responding to choices
 const init = () => {
     inquirer
         .prompt([
@@ -29,7 +29,9 @@ const init = () => {
                     "View all Employees",
                     "Add a department",
                     "Add a role",
-                    "Add an employee"
+                    "Add an employee",
+                    "Update an employee",
+                    "Log out"
                 ]
             }
         ]).then(ans => {
@@ -43,15 +45,20 @@ const init = () => {
                 case "Add a department" : addDept();
                     break;
                 case "Add a role" : addRole();
+                    break;
+                case "Add an employee" : addEmployee();
+                    break;
+                case "Update an employee role": updateEmployee();
+                    break;
+                case "Log out": endApp();
+                    break;
+
             }
 
-        })
-        
-        
-        
+        })      
     };      
     
-
+//function to view departments from DB
 const viewDept = () => {
     db.query(`SELECT * from department`, (err,result) => {
         err ? console.error(err) : console.table(result);
@@ -60,7 +67,7 @@ const viewDept = () => {
 
 };
 
-
+//function to view roles from DB
 const viewRoles = () => {
     db.query(`SELECT * from role`, (err,result) => {
         err ? console.error(err) : console.table(result);
@@ -68,6 +75,7 @@ const viewRoles = () => {
     })
 };
 
+//function to view employees from DB
 const viewEmployees = () => {
     db.query(`SELECT * from employee`, (err,result) => {
         err ? console.error(err) : console.table(result);
@@ -75,6 +83,8 @@ const viewEmployees = () => {
     })
 };
 
+
+//function to add Dept created by user - specifying name
 const addDept = () => {
     inquirer
         .prompt([
@@ -86,22 +96,23 @@ const addDept = () => {
         ]).then(ans => {
             db.query(`INSERT INTO department (name)
             VALUES(?) `, ans.addDept, (err,result) => {
-                err ? console.error(err) : console.table(result);
-                console.log('Department added');
+                err ? console.error(err) : console.log('Department added');
+                
                 init();
             } )
 
         })
 }
 
+
+//function to add Role created by user - specifying name and salary
 const addRole  = () => {
 
    db.query(`SELECT * from department`, (err,result) => {
-        const deptArr = result.map(pikachu => {
-            return pikachu.name;
+        const deptArr = result.map(array => {
+            return array.name;
             
         })
-        // console.log(deptArr);
         inquirer
         .prompt([
             {
@@ -121,14 +132,15 @@ const addRole  = () => {
                 choices: deptArr
             }
         ]).then(ans => {
-            var deptId;
+            //in this code I am grabbing the dept id  from the dept choice selected by user
+            var deptId; 
             for(var i = 0;  i < deptArr.length; i++) {
                 if(result[i].name === ans.roleDept) {
                     deptId = result[i].deptId;
                 }
 
             }
-
+            //inserting the new role into the DB using newly created name and salary and the dept id captured in above code
             db.query(`INSERT INTO role(title, salary, department_id) VALUES (?,?,?)`, [ans.roleName, ans.roleSalary, deptId], (err,result) => {
                 console.log("\n")
                 err ? console.error(err) : console.log("Role added.");
@@ -140,21 +152,50 @@ const addRole  = () => {
           
             
         })
-
-
-
     });
-    
-
-
- 
-
-
-
-
-
   
 };
+//function to add an employee
+const addEmployee = () => {
+
+   db.query(`SELECT * from role`, (err,result) => {
+    const roleArr = result.map(array => {
+        return array.name;
+    })
+   })
+            
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: "What is the employee's first name?",
+            name: 'firstName'
+        },
+        {
+            type: 'input',
+            message: "What is the employee's last name?",
+            name: 'lastName'
+        },
+        
+    ]).then(ans => {
+        db.query(`INSERT INTO employee(first_name, last_name) VALUES (?,?)`, [ans.firstName, ans.lastName], (err, results) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log('Employee successfully Added.')
+                init();
+            }
+        })
+       
+    })
+
+}
+
+//Log out function
+const endApp = () => {
+    console.log("Thanks for visiting the Employee Tracker!");
+    process.exit();
+}
 
 
 init();
